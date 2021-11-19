84e0ad6751f0d27dbc670b0c5b187e2746b16675
uchikuch <ucheazinge@gmail.com>
2021-11-19 11:33:49 +0100

    Customise tailwind config

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <div>
       <header>
-        <nav className="container"> Hello Nav</nav>
+        <nav className="container">
+          <h1 className="text-bookmark-purple">Hello Navigator</h1>
+        </nav>
       </header>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      primary: "#d2367a",
+      colors: {
+        "topset-pink": "#d2367a",
+        "bookmark-purple": "#5267Df",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#F7F7F7",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
     },
   },
   variants: {
